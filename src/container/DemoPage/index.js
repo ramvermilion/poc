1f438258836306388df112f09a8084dd1ba0fd87
B S Ramkumar <bsramku@publicisgroupe.net>
2022-05-19 10:55:24 +0530

    Adding Router

---
+++ src/container/DemoPage/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DemoPage(props) {
+    return (
+        <div>
+            <h1>This is a Demopage</h1>
+        </div>
+    );
+}
+
+export default DemoPage;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 //css imports
 import "antd/dist/antd.css";
-import './index.css';
-import App from './container/App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import App from "./container/App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
